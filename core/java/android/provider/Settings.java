14a81cce2dca803fa7e57283f30c822614cc5b70
cristianomatos <cristianobmatos@gmail.com>
2013-10-15 14:02:08 -0300

    Revert "Navigation ring custom shortcuts (1/2)" this commit e0fc102473bd0f20e0f782f18e516750cd6c0c89
    Change-Id: Ia3f5cc3416009ebe952b0d154a9e8f6945b1d4bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3183,17 +3183,6 @@ public final class Settings {
          */
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";  
 
-        /**
-         * Custom navring actions
-         *
-         * @hide
-         */
-        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
-            "navigation_ring_targets_0",
-            "navigation_ring_targets_1",
-            "navigation_ring_targets_2",
-        };
-
 	/**
          * whether which Ram Usage Bar mode is used on recent switcher
          * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
